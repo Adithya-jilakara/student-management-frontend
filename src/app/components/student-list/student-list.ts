fd8646e21f4a1311f073ef253122e12ad691d9bd
Adithya Jilakara <adithyajilakara@gmail.com>
2026-06-26 10:32:16 +0530

    Add Student List page with dummy data

--- src/app/components/student-list/student-list.ts
+++ src/app/components/student-list/student-list.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Student } from '../../models/student';
 
 @Component({
   selector: 'app-student-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student-list.html',
-  styleUrl: './student-list.css',
+  styleUrl: './student-list.css'
 })
-export class StudentList {}
+export class StudentList {
+  students: Student[] = [
+    { id: 1, name: 'Adithya', email: 'adithya@gmail.com', department: 'IT', age: 22 },
+    { id: 2, name: 'Ravi', email: 'ravi@gmail.com', department: 'CSE', age: 21 },
+    { id: 3, name: 'Kiran', email: 'kiran@gmail.com', department: 'ECE', age: 23 }
+  ];
+}
